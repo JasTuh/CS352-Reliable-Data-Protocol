6172b5f6ec7f4f305d61a402ff4f3af3a283457d
tom-kauk <tkaukonen1@gmail.com>
2017-10-23 17:17:51 -0400

    modified send and rec methods

--- sock352.py
+++ sock352.py
@@ -19,7 +19,8 @@ class socket:
     def bind(self,address):
         return 
 
-    def connect(self,address):  # fill in your code here 
+    def connect(self,address): 
+        # fill in your code here 
         return 
     
     def listen(self,backlog):
@@ -29,17 +30,36 @@ class socket:
         (clientsocket, address) = (1,1)  # change this to your code 
         return (clientsocket,address)
     
+    
     def close(self):   # fill in your code here 
         return 
 
     def send(self,buffer):
-        bytessent = 0     # fill in your code here 
-        return bytesent 
+        bytes_sent = 0     # fill in your code here 
+        # not sure what to do about self.sock, part of init
+        while bytes_sent < buffer:
+            sent = self.sock.send(msg[bytes_sent:])  
+            if sent == 0:
+                raise RuntimeError("socket connection broken")
+            bytes_sent = bytes_sent + sent
+
+        return bytes_sent 
 
     def recv(self,nbytes):
-        bytesreceived = 0     # fill in your code here
-        return bytesreceived 
-
+        bytes_rec= 0     # fill in your code here
+        
+        chunks = []
+        
+        while bytes_rec < nbytes:
+            chunk = self.sock.recv(min(nbytes - bytes_recd, 2048))
+            if chunk == '':
+                raise RuntimeError("socket connection broken")
+            chunks.append(chunk)
+            bytes_rec = bytes_rec + len(chunk)
+        return ''.join(chunks)
+        
+        
+      
 
     
 
