25a21615e7ad20f24a93a5ba595ebef099b82be9
Jason Tuhy <jasontuhy@gmail.com>
2017-10-23 20:30:20 -0400

    Add setup listen/bind/connect commands

--- sock352.py
+++ sock352.py
@@ -8,25 +8,35 @@ import sys
 # define the UDP ports all messages are sent
 # and received from
 
+socket = None
+UDPportTx = None
+UDPportRx = None
 def init(UDPportTx,UDPportRx):   # initialize your UDP socket here 
-    pass 
-    
+    socket = syssock.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    UDPportTx = UDPportTx 
+    UDPportRx = UDPportRx
 class socket:
-    
     def __init__(self):  # fill in your code here 
-        return
-    
-    def bind(self,address):
-        return 
+        if not socket:
+            print "Please run Sock352.init(UDPportTx, UDPportRx)"
+        self.address = None
+        self.backlog = 0
 
+    def bind(self,address):
+        self.address = address
+        socket.bind(address)
+
+#TODO Connection handshake
     def connect(self,address):  # fill in your code here 
         return 
     
     def listen(self,backlog):
-        return
+        self.backlog = backlog
+        socket.listen(backlog)
 
+#TODO Connection handshake
     def accept(self):
-        (clientsocket, address) = (1,1)  # change this to your code 
+        (clientsocket, address) = socket.accept()  # change this to your code 
         return (clientsocket,address)
     
     def close(self):   # fill in your code here 
@@ -39,8 +49,3 @@ class socket:
     def recv(self,nbytes):
         bytesreceived = 0     # fill in your code here
         return bytesreceived 
-
-
-    
-
-
