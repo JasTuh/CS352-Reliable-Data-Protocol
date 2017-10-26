93c65fc7de7b38ac4605e440e13ee95a006182fe
Jason Tuhy <jasontuhy@gmail.com>
2017-10-26 16:35:17 -0400

    Connection handshake now works

--- sock352.py
+++ sock352.py
@@ -2,48 +2,96 @@ import binascii
 import socket as syssock
 import struct
 import sys
+import random
 
 # these functions are global to the class and
 # define the UDP ports all messages are sent
 # and received from
 
-socket = None
-UDPportTx = None
-UDPportRx = None
+sock = None
+UDPportT = None
+UDPportR = None
+
+class Flags:
+    SYN, FIN, ACK, RESET, HAS_OPT = [0x01, 0x02, 0x04, 0x08, 0xA0]
+
 def init(UDPportTx,UDPportsRx):   # initialize your UDP socket here 
-    socket = syssock.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    UDPportTx = UDPportTx 
-    UDPportRx = UDPportRx
+    global UDPportT
+    global UDPportR 
+    global sock
+    UDPportT = int(UDPportTx)
+    UDPportR = int(UDPportsRx)
+    sock = syssock.socket(syssock.AF_INET, syssock.SOCK_DGRAM)
+    sock.settimeout(0.2)
+
+def make_header(flags, sequence_no, ack_no, window, payload_len, checksum=0, version=0x1, protocol=0, opt_ptr=0, source_port = 0, dest_port=0):
+    udpPkt_hdr_data = struct.Struct('!BBBBHHLLQQLL')
+    print (version, flags, opt_ptr, protocol, 40, checksum, source_port, dest_port, sequence_no, ack_no, window, payload_len)
+    return udpPkt_hdr_data.pack(version, flags, opt_ptr, protocol, 40, checksum, source_port, dest_port, sequence_no, ack_no, window, payload_len)
+
+def unpack_header(header):
+    udpPkt_hdr_data = struct.Struct('!BBBBHHLLQQLL')
+    return udpPkt_hdr_data.unpack(header)
+
+
 class socket:
-    def __init__(self):  # fill in your code here 
+    def __init__(self):
 #TODO discuss fields we need in our class so it doesn't become a mess 
-        if not socket:
+        if not sock:
             print "Please run Sock352.init(UDPportTx, UDPportRx)"
         self.address = None
         self.backlog = 0
+        self.first_sequence_no = None
 
     def bind(self,address):
-        self.address = address
-        socket.bind(address)
+        newAddress = (address[0], int(address[1])) #Port sent in as str so convert it
+        self.address = newAddress
+        sock.bind(newAddress)
 
-#TODO Connection handshake
-    def connect(self,address):  # fill in your code here 
-#TODO MAKE HEADERS using like the struct library
-        sock.sendto("", address)
-#TODO HANDSHAKE 
-        return 
-    
+    def get_header(self):
+        flags = Flags.SYN
+        self.first_sequence_no = random.randint(0,9999)
+        ack_no = 0
+        window = 20
+        payload_len = 40
+        header = make_header(flags, self.first_sequence_no, ack_no, window, payload_len)
+        return header
+
+    def connect_handshake(self):
+        data = None
+        tries = 0
+        while tries < 20:
+            try: 
+                data = sock.recv(1024)
+                break;
+            except syssock.timeout:
+                tries += 1
+                sock.sendto(header, newAddress)
+        if (tries == 20):
+            return False
+        (version, flags, opt_ptr, protocol, header_len, checksum, source_port, dest_port, sequence_no, ack_no, window, payload_len) = unpack_header(data)
+        if (flags == (Flags.SYN | Flags.ACK)):
+            print "Connection Established!"
+            return True
+        return False
+
+    def connect(self,address):
+        header = self.get_header()
+        self.address = (address[0], int(address[1])) 
+        sock.sendto(header, self.address)
+        return self.connect_handshake()
+
     def listen(self,backlog):
-#TODO learn what we should do in this method
         self.backlog = backlog
-        socket.listen(backlog)
 
     def accept(self):
-        (clientsocket, address) = socket.accept()  # change this to your code 
-#TODO Connection handshake
-        return (clientsocket,address)
-    
-    
+        sock.setblocking(1)
+        (header, addr) = sock.recvfrom(1024)  # change this to your code 
+        (version, flags, opt_ptr, protocol, header_len, checksum, source_port, dest_port, sequence_no, ack_no, window, payload_len) = unpack_header(header)
+        self.sequence_no = random.randint(0,99999)
+        replyHeader = make_header(Flags.SYN | Flags.ACK, self.sequence_no, sequence_no+1, window, 40) 
+        sock.sendto(replyHeader, addr)
+        return (sock, addr)
     def close(self):   # fill in your code here 
 #TODO TEAR DOWN
         return 
